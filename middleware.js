d2f86b536f031cd78e63292768febce8de15390f
ravi-rathod-ops <workforravirathod@gmail.com>
2025-03-11 12:28:43 +0530

    Fix Server Error

--- middleware.js
+++ middleware.js
@@ -47,7 +47,7 @@ export default createMiddleware(aj, clerk);
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:[^?]*$)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
